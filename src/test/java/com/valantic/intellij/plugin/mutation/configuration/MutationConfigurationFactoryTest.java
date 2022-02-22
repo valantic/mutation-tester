82f784092983e3052ae724bfee44ba39ebc8eb5e
hues6 <>
2022-02-22 19:19:38 +0100

    MutationConfigurationFactoryTest

---
+++ src/test/java/com/valantic/intellij/plugin/mutation/configuration/MutationConfigurationFactoryTest.java
@@ -0,0 +1,17 @@
+
+
+import de.hybris.bootstrap.annotations.UnitTest;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+@UnitTest
+@RunWith(MockitoJUnitRunner.class)
+public class MutationConfigurationFactoryTest {
+
+@org.mockito.InjectMocks
+private com.valantic.intellij.plugin.mutation.configuration.MutationConfigurationFactory underTest;
+
+
+}
